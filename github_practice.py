775a0e40982ccedf988a16fea5b80bfefc110c49
Ryan Dewey <145182587+RyanDewey@users.noreply.github.com>
2024-04-30 11:16:05 -0700

    added two files to practice GitHub Desktop

---
+++ github_practice.py
@@ -0,0 +1,12 @@
+
+# This is a practice file being used to practice using 
+# Github and Github desktop as well as the git commands
+
+name = "Ryan"
+age = 19
+school = "Saddleback"
+
+info = [name, age, school]
+
+for i in info:
+    print(i)
---
+++ index.js
@@ -0,0 +1,9 @@
+
+let printNums = (i) => {
+    for (let x = 0; x < i; x++)
+    {
+        console.log(x)
+    }
+}
+let x = 10
+printNums(x)
